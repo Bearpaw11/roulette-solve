1408e6b094f218372009c77105d79952177b1b83
Bearpaw11 <chrisbehrens@yahoo.com>
2024-02-02 12:43:23 -0700

    inital files

---
+++ src/main/java/com/cb/roulettesolve/RouletteSolveApplication.java
@@ -0,0 +1,13 @@
+package com.cb.roulettesolve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteSolveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cb/roulettesolve/RouletteSolveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.roulettesolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteSolveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
